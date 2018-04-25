213e157a9508a8970d0439d836e133e9db1cf3b6
leonwvink <38214418+leonwvink@users.noreply.github.com>
2018-04-25 11:33:01 +0200

    Wardrobe, open/close/in/out/kick/postmapping in name

---
+++ src/main/java/com/capgemini/demoWardrobe/controller/WardrobeController.java
@@ -0,0 +1,85 @@
+package com.capgemini.demoWardrobe.controller;
+
+import com.capgemini.demoWardrobe.model.Wardrobe;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/wardrobe")
+public class WardrobeController {
+    private static ArrayList<String> action = new ArrayList<>();
+    private Wardrobe wardrobe;
+
+    @GetMapping("/")
+    public ArrayList<String> action(){
+        return action;
+    }
+
+
+//    @GetMapping("/{name}")
+//    public void createWardrobe(@PathVariable String name) {
+//       wardrobe = new Wardrobe(name, false, false, 100);
+//       action.add(name + " is added");
+//    }
+
+
+
+    @GetMapping("/open")
+    public void open(){
+
+        if (wardrobe.isOpen() == false) {
+            wardrobe.setOpen(true);
+            action.add("Opening the door");
+        } else {
+            action.add("Door is already open!");
+        }
+    }
+
+    @GetMapping("/close")
+    public void close(){
+        if (wardrobe.isOpen() == true) {
+            wardrobe.setOpen(false);
+            action.add("Closing the door");
+        } else {
+            action.add("Door is already closed!");
+        }
+    }
+
+    @GetMapping("/getin")
+    public void getIn(){
+        System.out.println("1");
+        if (wardrobe.isInside() == false) {
+            wardrobe.setInside(true);
+            System.out.println("2");
+            action.add("Getting into the closet");
+        } else {
+            action.add("You are already in!");
+        }
+    }
+
+    @GetMapping("/getout")
+    public void getOut(){
+        if (wardrobe.isInside() == true) {
+            wardrobe.setInside(false);
+            action.add("Getting out of the closet");
+        } else {
+            action.add("You are already out of the closet");
+        }
+    }
+
+    @GetMapping("/kick")
+    public void kick(int force){
+        wardrobe.setHealth(wardrobe.getHealth() - force);
+        System.out.println("You kicked the wardrobe with " + force + " force! The wardrobe has still a health of " + (wardrobe.getHealth()-force) );
+        action.add("Kicking the wardrobe");
+    }
+
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public Wardrobe createWardrobe(Wardrobe wardrobe2) {
+        wardrobe = new Wardrobe(wardrobe2.getName(), false, false,100);
+        action.add(wardrobe.getName() + " is added.");
+        return wardrobe;
+    }
+
+}
---
+++ src/main/resources/static/registry.js
@@ -0,0 +1,83 @@
+
+function getWardrobe(){
+$.get("/api/wardrobe/", function(data){
+    if (data.length <= 0) {
+    return;
+    }
+
+    var html = "";
+
+    for (var i=0; i<data.length; i++){
+        html += "<p>" + data[i] + "</p>";
+    }
+
+    $("#log").html(html);
+});
+}
+
+
+function saveName(e){
+    e.preventDefault();
+    var name = $("#name").val();
+    $.get("/api/wardrobe/" + name, function() {
+    getWardrobe();
+    });
+}
+
+function saveName(e) {
+    e.preventDefault();
+    var name1 = $("#name").val();
+
+    $.post("api/wardrobe/add",
+    {
+    name: name1
+    },
+    function() {
+    getWardrobe();
+    } );
+}
+
+function open(){
+
+    $.get("/api/wardrobe/open", function() {
+    getWardrobe();
+    });
+}
+
+function close(){
+
+    $.get("/api/wardrobe/close", function() {
+    getWardrobe();
+    });
+}
+
+function getIn(){
+
+    $.get("/api/wardrobe/getin", function() {
+    getWardrobe();
+    });
+}
+
+function getOut(){
+
+    $.get("/api/wardrobe/getout", function() {
+    getWardrobe();
+    });
+}
+
+function kick(){
+
+    var force = $("#force").val();
+    $.get("/api/wardrobe/kick", function() {
+    getWardrobe();
+    });
+}
+
+
+$("#add").click(saveName);
+$("#open").click(open);
+$("#close").click(close);
+$("#getIn").click(getIn);
+$("#getOut").click(getOut);
+$("#kick").click(kick);
+getWardrobe();
